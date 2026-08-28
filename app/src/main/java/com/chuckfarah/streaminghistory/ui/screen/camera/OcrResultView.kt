6a479595f72f4c80cec0b73dd7e0cca2ceef7012
Chuck Farah <chuckfarah@gmail.com>
2026-08-28 09:23:01 -0400

    fix(camera): add missing background import

--- app/src/main/java/com/chuckfarah/streaminghistory/ui/screen/camera/OcrResultView.kt
+++ app/src/main/java/com/chuckfarah/streaminghistory/ui/screen/camera/OcrResultView.kt
@@ -1,5 +1,6 @@
 package com.chuckfarah.streaminghistory.ui.screen.camera
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
